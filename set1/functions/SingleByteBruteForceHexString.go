ea0861d39b7a119775b932644d3bcbdf9968ff82
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-22 13:35:48 -0700

    challenge 3 complete

--- set1/functions/SingleByteBruteForceHexString.go
+++ set1/functions/SingleByteBruteForceHexString.go
@@ -1,13 +1,11 @@
 package set1
 
-import (
-	"encoding/hex"
-	"fmt"
-)
+import "encoding/hex"
 
 // SingleByteBruteForceHexString takes an encrypted hex string, xors it with different byte combinations
 func SingleByteBruteForceHexString(hexStringMsg string) string {
 	var ret []byte
+	finalInd := 0.0
 
 	// convert hex string to bytes
 	byteMsg, _ := hex.DecodeString(hexStringMsg)
@@ -18,11 +16,34 @@ func SingleByteBruteForceHexString(hexStringMsg string) string {
 		for _, bytes := range byteMsg {
 			xoredBytes = append(xoredBytes, bytes^byte(oneByte))
 		}
-		fmt.Println(string(xoredBytes[:]))
-		fmt.Println()
+		tempInd := CalcCharFrequency(string(xoredBytes[:]))
+		if tempInd > finalInd {
+			ret = xoredBytes
+			finalInd = tempInd
+		}
 	}
 
-	// xor byte with byteMsg
-	// endloop
 	return string(ret[:])
+}
+
+// CalcCharFrequency takes a string, and returns the Index of Coincidence of that string
+func CalcCharFrequency(s string) float64 {
+	indOfCo := 0.0
+	letterCount := 0
+
+	charCount := make(map[rune]int)
+
+	for _, char := range s {
+		if ('A' <= (char) && (char) <= 'Z') || ('a' <= (char) && (char) <= 'z') || ((char) == ' ') {
+			charCount[char]++
+		}
+		letterCount++
+	}
+
+	for _, count := range charCount {
+		indOfCo += float64(count * (count - 1))
+	}
+	indOfCo /= float64(letterCount * (letterCount - 1))
+
+	return indOfCo
 }
