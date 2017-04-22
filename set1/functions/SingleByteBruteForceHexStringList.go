7703dbeb96d22c9ff1969b56aabf34f26665d007
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-22 14:03:30 -0700

    added untracked files

---
+++ set1/functions/SingleByteBruteForceHexStringList.go
@@ -0,0 +1,27 @@
+package set1
+
+import (
+	"bufio"
+	"os"
+)
+
+// SingleByteBruteForceHexStringList takes a filename, opens a file of hex strings, returns encrypted string with index of coincidence
+func SingleByteBruteForceHexStringList(fileLoc string) (string, float64) {
+	finalStr, finalInd := "", 0.0
+
+	file, err := os.Open(fileLoc)
+	if err != nil {
+		panic(err)
+	}
+
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		tempStr, tempInd := SingleByteBruteForceHexString(input.Text())
+		if tempInd > finalInd {
+			finalStr = tempStr
+			finalInd = tempInd
+		}
+	}
+
+	return finalStr, finalInd
+}
