7d6f37c145edc0613771917568630953923a69ff
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-26 15:42:42 -0700

    cbc and ecb should be good

--- set2/functions/aesCbcCipherFunctions.go
+++ set2/functions/aesCbcCipherFunctions.go
@@ -36,22 +36,32 @@ func DecryptCbc(byteData, byteKey, byteIv []byte) []byte {
 }
 
 // EncryptCbc takes a file, and decrypts its data using the string key and hex string iv, by aes-128-cbc
-func EncryptCbc(fileLoc, key, iv string) {
-	// open file
-	// convert data into bytes
+func EncryptCbc(byteData, byteKey, byteIv []byte) []byte {
 
-	// check if iv and key sizes are equal
-	// if not equal return error
+	var finalCiphertext []byte
 
 	// initialize xor variable to be iv
-	// loop over the byte data
-	// take an iv-sized block of data
-	// xor the block with the xor variable using the xor function
-	// send the resultant value to the aes ecb function with the key
-	// take the ciphertext and append it to final encrypted byte array
-	// take the ciphertext again and assign it to the xor variable
-	// endloop
+	xorVar := byteIv
+	blockSize := len(byteIv)
 
-	// returned ciphertext in bytes
+	// loop over the byte data
+	for i := 0; i < len(byteData); i += blockSize {
+		// fmt.Println(i)
+		// take an iv-sized block of data
+		dataBlock := byteData[i : i+blockSize]
+		// take the decrypted text and xor it with the xor variable using the xor function
+		var ciphertext []byte
+		ciphertext = set1.XorByteArrays(dataBlock, xorVar)
+		// send the block to the aes ecb function with the key
+		ciphertext = set1.EncryptEcb(ciphertext, byteKey)
+		// take the iv-sized block of data and assign it to the xor variable
+		xorVar = ciphertext
+		// take the resultant plaintext and append it to final decrypted message byte array
+		finalCiphertext = append(finalCiphertext, ciphertext...)
+
+	} // endloop
+
+	// return plaintext in bytes
+	return finalCiphertext
 
 }
