76612cdcf0ad607eeac550b43bf44ff97a90c9ae
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-27 14:45:49 -0700

    random key generated

--- set2/functions/blockTypeDetection.go
+++ set2/functions/blockTypeDetection.go
@@ -1,16 +1,28 @@
 package set2
 
-import "github.com/goCryptography/set1/functions"
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/goCryptography/set1/functions"
+)
 
 // EncryptionOracle encrypts data, and states which block type it encrypted with
 func EncryptionOracle(byteData []byte) string {
-
-	byteKey := []byte("YELLOW SUBMARINE")
+	blockSize := 16
+	// byteKey := []byte("YELLOW SUBMARINE")
+	byteKey := make([]byte, blockSize)
+	_, err := rand.Read(byteKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("rand key: %v\n", byteKey)
+	// fmt.Println(string(byteData))
 
 	// encrypt by cbc
-	encryptedData := EncryptCbc(byteData, byteKey, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	// encryptedData := EncryptCbc(byteData, byteKey, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 	// encrypt by ecb
-	// encryptedData := set1.EncryptEcb(byteData, byteKey)
+	encryptedData := set1.EncryptEcb(byteData, byteKey)
 
 	// fmt.Println(encryptedData)
 	// encryptedData = set1.DecryptEcb(encryptedData, byteKey)
