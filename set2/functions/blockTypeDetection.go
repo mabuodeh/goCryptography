ccbdee667c205efb17ec675a0d1a4ffa67dc8454
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-27 18:07:34 -0700

    added random generators. Now for testing

--- set2/functions/blockTypeDetection.go
+++ set2/functions/blockTypeDetection.go
@@ -1,8 +1,8 @@
 package set2
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
 
 	"github.com/goCryptography/set1/functions"
 )
@@ -12,12 +12,54 @@ func EncryptionOracle(byteData []byte) string {
 	blockSize := 16
 	// byteKey := []byte("YELLOW SUBMARINE")
 	byteKey := make([]byte, blockSize)
-	_, err := rand.Read(byteKey)
+
+	// halfSize := *big.NewInt(5)
+	// temp, err := rand.Int(rand.Reader, &halfSize)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	temp := make([]byte, 1)
+	_, err := rand.Read(temp)
+	if err != nil {
+		panic(err)
+	}
+	randBegSize := int(temp[0])/256 + 10
+
+	temp = make([]byte, 1)
+	_, err = rand.Read(temp)
+	if err != nil {
+		panic(err)
+	}
+	randEndSize := int(temp[0])/256 + 10
+
+	begByte := make([]byte, randBegSize)
+	_, err = rand.Read(begByte)
+	if err != nil {
+		panic(err)
+	}
+	endByte := make([]byte, randEndSize)
+	_, err = rand.Read(endByte)
+	if err != nil {
+		panic(err)
+	}
+
+	byteData = append(begByte, byteData...)
+	byteData = append(byteData, endByte...)
+
+	_, err = rand.Read(byteKey)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("rand key: %v\n", byteKey)
 	// fmt.Println(string(byteData))
+
+	// add 5-10 random bytes to beginning and end of data
+	_, err = rand.Read(byteKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("beg rand key: %v\n", byteKey)
+	byteData = append(byteData)
 
 	// encrypt by cbc
 	// encryptedData := EncryptCbc(byteData, byteKey, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
