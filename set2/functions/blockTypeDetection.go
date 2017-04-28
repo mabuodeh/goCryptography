d9845a980280013495b5f9e056e836e075f3a7ec
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-27 18:23:08 -0700

    random generators working

--- set2/functions/blockTypeDetection.go
+++ set2/functions/blockTypeDetection.go
@@ -9,66 +9,55 @@ import (
 
 // EncryptionOracle encrypts data, and states which block type it encrypted with
 func EncryptionOracle(byteData []byte) string {
-	blockSize := 16
-	// byteKey := []byte("YELLOW SUBMARINE")
-	byteKey := make([]byte, blockSize)
+	//
+	// Create random bytes in the beginning and end, and create a random byte key
+	//
 
-	// halfSize := *big.NewInt(5)
-	// temp, err := rand.Int(rand.Reader, &halfSize)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	blockSize := 16
+
+	fmt.Printf("data before additions: %v\n", byteData)
+
+	// Create 1 random byte, convert it to an int between 5 and 10
 	temp := make([]byte, 1)
-	_, err := rand.Read(temp)
-	if err != nil {
-		panic(err)
-	}
-	randBegSize := int(temp[0])/256 + 10
+	_, _ = rand.Read(temp)
+
+	randBegSize := int((float64(temp[0])/256.0)*5) + 5
+	fmt.Printf("beginning size: %d\n", randBegSize)
 
 	temp = make([]byte, 1)
-	_, err = rand.Read(temp)
-	if err != nil {
-		panic(err)
-	}
-	randEndSize := int(temp[0])/256 + 10
+	_, _ = rand.Read(temp)
+
+	randEndSize := int((float64(temp[0])/256.0)*5) + 5
+	fmt.Printf("end size: %d\n", randEndSize)
 
 	begByte := make([]byte, randBegSize)
-	_, err = rand.Read(begByte)
-	if err != nil {
-		panic(err)
-	}
+	_, _ = rand.Read(begByte)
+	fmt.Printf("beginning byte array: %v\n", begByte)
+
 	endByte := make([]byte, randEndSize)
-	_, err = rand.Read(endByte)
-	if err != nil {
-		panic(err)
-	}
+	_, _ = rand.Read(endByte)
+	fmt.Printf("end byte array: %v\n", endByte)
 
 	byteData = append(begByte, byteData...)
 	byteData = append(byteData, endByte...)
 
-	_, err = rand.Read(byteKey)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("rand key: %v\n", byteKey)
-	// fmt.Println(string(byteData))
+	fmt.Printf("data after additions: %v\n", byteData)
 
-	// add 5-10 random bytes to beginning and end of data
-	_, err = rand.Read(byteKey)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("beg rand key: %v\n", byteKey)
-	byteData = append(byteData)
+	// Generate a random 16 byte byteKey
+	byteKey := make([]byte, blockSize)
+	_, _ = rand.Read(byteKey)
+
+	fmt.Printf("rand key: %v\n", byteKey)
+
+	//
+	// Take byte key and byte data, and encrypt with either ecb or cbc
+	//
 
 	// encrypt by cbc
 	// encryptedData := EncryptCbc(byteData, byteKey, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 	// encrypt by ecb
 	encryptedData := set1.EncryptEcb(byteData, byteKey)
 
-	// fmt.Println(encryptedData)
-	// encryptedData = set1.DecryptEcb(encryptedData, byteKey)
-	// fmt.Println(string(encryptedData))
 	// send encrypted data, check whether it's ecb or cbc
 	duplicates := set1.DetectAesEcbLine(encryptedData)
 
