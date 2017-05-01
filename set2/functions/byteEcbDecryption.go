9caef0fc5786b2306956878423272434470c357a
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-04-30 17:47:36 -0700

    set algorithm for challenge 12 part 1

---
+++ set2/functions/byteEcbDecryption.go
@@ -0,0 +1,13 @@
+package set2
+
+// BreakEcb
+funct BreakEcb() {
+  // part one
+  // use oracle function, but key must not be random
+  // take a base64 string,
+  // decode it,
+  // and append it to the plaintext
+  // encrypt using ECB
+
+  // part two
+}
