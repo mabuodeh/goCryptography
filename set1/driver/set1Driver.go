256f2a435e573127cc52fd772d76a6a598656e94
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-05-04 17:05:14 -0700

    splitting functions so that each go file is one module in the package

--- set1/driver/set1Driver.go
+++ set1/driver/set1Driver.go
@@ -71,15 +71,4 @@ func main() {
 	fmt.Println("Challenge 6")
 	fmt.Println(set1.BreakRepeatingKey(fileDirectory + "6.txt"))
 
-	fmt.Println("Challenge 7")
-	cha7ByteData := set1.OpenTextFile(fileDirectory + "7b.txt")
-	fmt.Println(set1.EncryptEcb(cha7ByteData, []byte("YELLOW SUBMARINE")))
-	cha7ByteData = set1.OpenBase64File(fileDirectory + "7.txt")
-	fmt.Println(string(set1.DecryptEcb(cha7ByteData, []byte("YELLOW SUBMARINE"))))
-
-	fmt.Println("Challenge 8")
-	cha8ByteData := set1.OpenHexFile(fileDirectory + "8.txt")
-	// fmt.Println(cha8ByteData)
-	fmt.Println(set1.DetectAesEcbLine(cha8ByteData))
-
 }
