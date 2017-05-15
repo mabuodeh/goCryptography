5ec4976129ae04a930a0a3391da51c4b7eca447d
Muhammed AbuOdeh <mabuodeh3@gmail.com>
2017-05-15 14:41:50 -0700

    no use. will try putting admin in its own block, as well as user

--- blockAndStreamCiphers/emailEncryption.go
+++ blockAndStreamCiphers/emailEncryption.go
@@ -18,27 +18,46 @@ func ModifyCredentials(email string) {
 	encryptedData := EncryptEcb([]byte(msg), key)
 
 	// hacking here
-	// form:
-	// email=adminfooba r@bar.com&uid=10 &role=user666666, length = 39
-	// ignore first 7 bytes, cut next 5 bytes (ignore email=, cut admin)
-	admin := encryptedData[6:11]
-	// pre := encryptedData[:6]
-	// post := encryptedData[11:]
-	fmt.Println(len(encryptedData))
-	fmt.Println(admin)
-	// encryptedData = append(encryptedData[0:6], encryptedData[11:]...)
+	// 3 blocks, 48 bytes
+	first := encryptedData[:16]
+	second := encryptedData[16:32]
+	third := encryptedData[32:]
 
-	fmt.Println(encryptedData)
-	// email=foo@bar.com&uid=10&role=user, length = 34
-	// subtract length of admin & user for paste beginning location (30)
 	final := make([]byte, 0)
-	final = append(final, encryptedData[:38]...)
-	// final = append(final, encryptedData[:43]...)
-	final = append(final, admin...)
-	final = append(final, encryptedData[42:]...)
-	fmt.Println(final)
-	// paste admin at 30
-	// email=foo@bar.com&uid=10&role=admin
+	final = append(final, first...)
+	final = append(final, second...)
+
+	admin := first[6:11]
+
+	mod := make([]byte, 0)
+	mod = append(mod, third[:6]...)
+	mod = append(mod, admin...)
+	mod = append(mod, third[11:]...)
+
+	final = append(final, mod...)
+	fmt.Println(first)
+	fmt.Println(mod)
+
+	// form:
+	// email=adminfooba
+	// r@bar.com&uid=10
+	// &role=user666666, length = 42 + 6
+	// ignore first 7 bytes, cut next 5 bytes (ignore email=, cut admin)
+	// admin := encryptedData[6:11]
+	// // pre := encryptedData[:6]
+	// // post := encryptedData[11:]
+	// fmt.Println(len(encryptedData))
+	// fmt.Println(admin)
+	// // encryptedData = append(encryptedData[0:6], encryptedData[11:]...)
+	//
+	// fmt.Println(encryptedData)
+	// // subtract length of admin & user for paste beginning location (30)
+	// final := make([]byte, 0)
+	// final = append(final, encryptedData[:40]...)
+	// // final = append(final, encryptedData[:43]...)
+	// final = append(final, admin...)
+	// final = append(final, encryptedData[43:]...)
+	// fmt.Println(final)
 
 	// decrypt data with key
 	plaintext := DecryptEcb(final, key)
